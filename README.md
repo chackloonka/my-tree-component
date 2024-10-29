9d79fdde724235f17b2627c076eb49a9f2ea7b06
Kate <catwinchester.ks@gmail.com>
2024-10-29 18:23:02 +0000

    init: set up project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tree Component Prototype 
+
+## Setup Instructions
+
+1. **Install dependencies**: `npm install`
+2. **Run the application**: `npm run dev`
+3. **Run tests**: `npm test`
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Tree from './components/Tree'
+
+function App() {
+  return (
+    <>
+      <h1>Tree Prototype Component</h1>
+      <Tree />
+    </>
+  )
+}
+
+export default App
