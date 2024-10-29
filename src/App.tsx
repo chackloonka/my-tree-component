1c23b7f34bc050fb65978b6d463bd9278b9368ff
Kate <catwinchester.ks@gmail.com>
2024-10-29 22:00:49 +0000

    add mockdata / Tree Component with recursive tree build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Tree from './components/Tree'
+import mockData from './mock/taxonomy.json'
 
 function App() {
   return (
     <>
       <h1>Tree Prototype Component</h1>
-      <Tree />
+      <Tree data={mockData} />
     </>
   )
 }
--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+/**
+ * Type definition for a ITreeNode.
+ * - `taxon`: Defines the taxonomic rank "Family" | "Genus" | "Species".
+ * - `name`: Scientific name of the taxon.
+ * - `common_name`: Common name of the taxon.
+ * - `children`: Optional array of child ITreeNodes.
+ */
+interface ITreeNode {
+  taxon: "Family" | "Genus" | "Species";
+  name: string;
+  common_name: string;
+  children?: ITreeNode[];
+}
 
-function Tree() {
+interface ITreeProps {
+  data: ITreeNode[];
+}
+
+function Tree({ data }: { data: ITreeProps }) {
+  console.log(data.data);
+
   return (
-    <div>Tree</div>
+    <div>
+      <ul>
+        {data.map((item: ITreeNode) =>
+          <TreeItem node={item} key={item.name} />
+        )}
+      </ul>
+    </div>
   )
+}
+
+function TreeItem({ node }: { node: ITreeNode }) {
+
+  return <li>
+    <span>{node.common_name}</span>
+    {node.children && <ul>
+      {node.children?.map(item =>
+        <TreeItem node={item} key={item.name} />
+      )}
+    </ul>}
+
+  </li>
 }
 
 export default Tree
