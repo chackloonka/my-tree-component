0e079c44b0db2c0c9e60bc1a06d3099166129bf6
Kate <catwinchester.ks@gmail.com>
2024-10-30 01:45:04 +0000

    add search/filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { Container } from 'react-bootstrap'
+import { Container, Form } from 'react-bootstrap'
 import Tree from './components/Tree'
 import mockData from './mock/taxonomy.json'
 import './index.css'
+import { useState } from 'react';
 
 function App() {
+  const [query, setQuery] = useState("");
+
   return (
     <Container>
-      <h1 className="my-4">Tree Prototype Component</h1>
-      <Tree data={mockData} />
+      <h1 className="my-4">Tree Prototype</h1>
+      <Form.Group className='my-3'>
+        <Form.Label>Search</Form.Label>
+        <Form.Control value={query} onChange={(e) => setQuery(e.target.value)}/>
+      </Form.Group>
+
+      <Tree data={mockData} query={query} />
     </Container>
   )
 }
--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import {
   FaRegFolder,
@@ -17,35 +17,67 @@ interface ITreeNode {
   common_name: string;
   children?: ITreeNode[];
 }
-
+/**
+ * Props for Tree component.
+ * - `data`: Node data to be rendered in the tree structure.
+ * - `searchQuery`: Optional string for searching/filtering nodes.
+ */
 interface ITreeProps {
   data: ITreeNode[];
+  query?: string;
 }
 
-function Tree({ data }: { data: ITreeProps }) {
+function Tree({ data, query } : ITreeProps) {
   return (
     <ListGroup>
       {data.map((item: ITreeNode) =>
-        <TreeItem node={item} key={item.name} />
+        <TreeItem node={item} key={item.name} query={query}/>
       )}
     </ListGroup>
   )
 }
 
-function TreeItem({ node }: { node: ITreeNode }) {
+/**
+ * Props for Tree Item Component.
+ * - `node`: Node data to be rendered in the tree structure.
+ * - `query`: Optional string for searching/filtering nodes.
+ */
+interface ITreeItemProps {
+  node: ITreeNode;
+  query?: string;
+}
+const filterTree = (node:ITreeNode, query:string): boolean => {
+  if(
+    node.common_name.toLowerCase().includes(query.toLowerCase()) ||
+    node.name.toLowerCase().includes(query.toLowerCase()) 
+  ) return true;
+  if(node.children) {
+    return node.children.some((child) => filterTree(child, query))
+  }
+  return false
+}
+
+function TreeItem({ node, query } : ITreeItemProps) {
   const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+    if (query) {
+      setIsOpen(filterTree(node, query));
+    } else {
+      setIsOpen(false);
+    }
+  }, [query, node]);
+
+  // Skip rendering nodes that don't match the search
+  if (query && !filterTree(node, query)) return null;
 
   return <>
     <div className="TreeItem">
-      <ListGroupItem 
-        className="cursor-pointer" 
-        onClick={() => setIsOpen(!isOpen)}
-      >
+      <ListGroupItem  onClick={() => setIsOpen(!isOpen)} >
         <span className="pe-2">
           {node.children && (isOpen ? <FaRegFolderOpen /> : <FaRegFolder />)}
         </span>
         <span>
-          {node.common_name} aka {node.name}
+          {node.taxon} "{node.common_name} aka {node.name}"
         </span>
       </ListGroupItem>
       {isOpen && node.children && node.children?.map(item =>
