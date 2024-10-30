ee0b61c085910a302f355220f4e7110e0cc7ad04
Kate <catwinchester.ks@gmail.com>
2024-10-30 00:41:07 +0000

    add ability to toggle list items / add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Container } from 'react-bootstrap'
 import Tree from './components/Tree'
 import mockData from './mock/taxonomy.json'
+import './index.css'
 
 function App() {
   return (
-    <>
-      <h1>Tree Prototype Component</h1>
+    <Container>
+      <h1 className="my-4">Tree Prototype Component</h1>
       <Tree data={mockData} />
-    </>
+    </Container>
   )
 }
 
--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import {
+  FaRegFolder,
+  FaRegFolderOpen,
+} from "react-icons/fa";
 /**
- * Type definition for a ITreeNode.
- * - `taxon`: Defines the taxonomic rank "Family" | "Genus" | "Species".
+ * ITreeNode:
+ * - `taxon`: Defines the taxonomic rank: "Family" | "Genus" | "Species".
  * - `name`: Scientific name of the taxon.
  * - `common_name`: Common name of the taxon.
  * - `children`: Optional array of child ITreeNodes.
@@ -17,30 +23,36 @@ interface ITreeProps {
 }
 
 function Tree({ data }: { data: ITreeProps }) {
-  console.log(data.data);
-
   return (
-    <div>
-      <ul>
-        {data.map((item: ITreeNode) =>
-          <TreeItem node={item} key={item.name} />
-        )}
-      </ul>
-    </div>
+    <ListGroup>
+      {data.map((item: ITreeNode) =>
+        <TreeItem node={item} key={item.name} />
+      )}
+    </ListGroup>
   )
 }
 
 function TreeItem({ node }: { node: ITreeNode }) {
+  const [isOpen, setIsOpen] = useState(false)
 
-  return <li>
-    <span>{node.common_name}</span>
-    {node.children && <ul>
-      {node.children?.map(item =>
+  return <>
+    <div className="TreeItem">
+      <ListGroupItem 
+        className="cursor-pointer" 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="pe-2">
+          {node.children && (isOpen ? <FaRegFolderOpen /> : <FaRegFolder />)}
+        </span>
+        <span>
+          {node.common_name} aka {node.name}
+        </span>
+      </ListGroupItem>
+      {isOpen && node.children && node.children?.map(item =>
         <TreeItem node={item} key={item.name} />
       )}
-    </ul>}
-
-  </li>
+    </div>
+  </>
 }
 
 export default Tree
